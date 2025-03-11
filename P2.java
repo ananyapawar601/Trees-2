b97e1cf890fd359316e2669a9a1fd7d26530e0be
Ananya Pawar <avp6363@psu.edu>
2025-03-11 19:43:31 -0400

    P2 done

--- P2.java
+++ P2.java
@@ -1,3 +1,23 @@
-public class P2 {
+class Solution {
+    int result;
     
+    public int sumNumbers(TreeNode root) {
+        helper(root, 0);
+        return result;
+    }
+
+    private void helper(TreeNode root, int curr) {
+        //base case
+        if (root == null) return;
+
+        //logic
+        curr = curr * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            result += curr;
+        }
+        helper(root.left, curr);
+
+        helper(root.right, curr);
+    }
 }
+
